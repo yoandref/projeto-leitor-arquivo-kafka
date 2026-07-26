0618a87af9af2593008a794cd534755d19e0cd49
devandref <andredepaulacontato@gmail.com>
2026-07-25 21:17:04 -0300

    include: primeira aplicação

---
+++ leitor-arquivo/src/main/java/io/github/devandref/leitor_arquivo/LeitorArquivoApplication.java
@@ -0,0 +1,13 @@
+package io.github.devandref.leitor_arquivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitorArquivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeitorArquivoApplication.class, args);
+	}
+
+}
---
+++ leitor-arquivo/src/test/java/io/github/devandref/leitor_arquivo/LeitorArquivoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.devandref.leitor_arquivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitorArquivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
